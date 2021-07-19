15b8f114f2eb93ab7a7d417a13607242efe34158
macong0503 <1512346520@qq.com>
2021-07-19 15:34:26 +0800

    Create ouArrayListDemo.java

---
+++ work/ouArrayListDemo.java
@@ -0,0 +1,12 @@
+package com.hqyj.mc.work;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ouArrayListDemo {
+    public static void main(String[] args) {
+     ouArrayList o = new ouArrayList();
+     o.ouList();
+    }
+}
