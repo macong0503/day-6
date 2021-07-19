530c48f276061ac1c46375484bee78a434ad2d86
macong0503 <1512346520@qq.com>
2021-07-19 15:33:53 +0800

    Create PackagingDemoText.java

---
+++ Text/PackagingDemoText.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.Text;
+
+public class PackagingDemoText {
+    public static void main(String[] args) {
+        PackagingDemo pd = new PackagingDemo();
+        pd.setName("张三");
+        pd.setAge(160);
+        pd.show();
+    }
+}
