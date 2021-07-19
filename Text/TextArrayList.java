4df459164f674b9e07fc60d9c34fd93f48f2c5d2
macong0503 <1512346520@qq.com>
2021-07-19 15:34:09 +0800

    Create TextArrayList.java

---
+++ Text/TextArrayList.java
@@ -0,0 +1,12 @@
+package com.hqyj.mc.Text;
+
+import java.util.ArrayList;
+
+public class TextArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> Arr = new ArrayList<>();
+        Arr.add(1);
+        Arr.add(2);
+        System.out.println(Arr);
+    }
+}
